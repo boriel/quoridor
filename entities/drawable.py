b8dc22f0e5fd3fd7be8804e97283edfd3ca52f3e
Jose Rodriguez <boriel@gmail.com>
2020-01-13 15:18:09 +0100

    Add Drawable entity
    Base game entity for all ones that can be displayed on
    the screen.

---
+++ entities/drawable.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import abc
+import pygame
+
+
+class Drawable(abc.ABC):
+    """ Abstract drawable class. Implements a generic object that can be
+    painted in the screen.
+    """
+    def __init__(self,
+                 screen,
+                 color=None,
+                 border_color=None,
+                 border_size=None):
+        self.screen = screen  # screen to paint to
+        self.color = color
+        self.border_color = border_color
+        self.border_size = border_size
+
+    def draw(self):
+        if self.color is None or self.border_color is None:
+            return
+
+        if self.border_size is None:
+            return
+
+        r = self.rect
+        if r is None:
+            return
+
+        pygame.draw.rect(self.screen, self.color, r, 0)
+        pygame.draw.rect(self.screen, self.border_color, r, self.border_size)
+
+    @property
+    @abc.abstractmethod
+    def rect(self):
+        """ Must be overloaded by children classes
+        """
+        return None
